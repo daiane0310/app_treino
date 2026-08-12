606a3b46f446fc847b7e7a2d0e7dadbffa9c2750
Daiane Souza - Master Internet <daiane.souza@soumaster.com.br>
2026-08-12 16:12:43 -0300

    Configura persistência de usuários com JPA e PostgreSQL

---
+++ src/main/java/com/apptreino/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.apptreino.repository;
+
+import com.apptreino.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
