bfbf3504384c814678ceb05f3d6bf7d6bcd42b1e
Daiane Souza - Master Internet <daiane.souza@soumaster.com.br>
2026-08-14 11:04:48 -0300

    Adiciona rota de teste JWT

---
+++ src/main/java/com/apptreino/controller/TesteAuthController.java
@@ -0,0 +1,18 @@
+package com.apptreino.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class TesteAuthController {
+
+    @GetMapping("/teste")
+    public ResponseEntity<Map<String, String>> testarAutenticacao() {
+        return ResponseEntity.ok(Map.of("mensagem", "Token válido"));
+    }
+}
